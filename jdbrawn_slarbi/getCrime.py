f23c66701a7f93dedf1a835254441c293987a5ea
David Brawn <david.brawn@gmail.com>
2017-09-29 16:01:23 -0400

    added provenance

--- jdbrawn_slarbi/getCrime.py
+++ jdbrawn_slarbi/getCrime.py
@@ -33,3 +33,43 @@ class getCrime(dml.Algorithm):
         endTime = datetime.datetime.now()
 
         return {"start": startTime, "end": endTime}
+
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        '''
+            Create the provenance document describing everything happening
+            in this script. Each run of the script will generate a new
+            document describing that invocation event.
+            '''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('jdbrawn_slarbi', 'jdbrawn_slarbi')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont',
+                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        doc.add_namespace('bdp', 'https://data.boston.gov/datastore/')
+
+        this_script = doc.agent('alg:jdbrawn_slarbi#getCrime',
+                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        resource = doc.entity('12cb3883-56f5-47de-afa5-3b1cf61b257b',
+                              {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource',
+                               'ont:Extension': 'json'})
+        get_crime = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAssociatedWith(get_crime, this_script)
+        doc.usage(get_crime, resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval'})
+
+        crime = doc.entity('dat:jdbrawn_slarbi#crime',
+                          {prov.model.PROV_LABEL: 'Boston Crime', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(crime, this_script)
+        doc.wasGeneratedBy(crime, get_crime, endTime)
+        doc.wasDerivedFrom(crime, resource, get_crime, get_crime, get_crime)
+
+        repo.logout()
+
+        return doc
--- jdbrawn_slarbi/getStreetlights.py
+++ jdbrawn_slarbi/getStreetlights.py
@@ -33,3 +33,43 @@ class getStreetlights(dml.Algorithm):
         endTime = datetime.datetime.now()
 
         return {"start": startTime, "end": endTime}
+
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        '''
+            Create the provenance document describing everything happening
+            in this script. Each run of the script will generate a new
+            document describing that invocation event.
+            '''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('jdbrawn_slarbi', 'jdbrawn_slarbi')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont',
+                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        doc.add_namespace('bdp', 'https://data.boston.gov/datastore/')
+
+        this_script = doc.agent('alg:jdbrawn_slarbi#getStreetlights',
+                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        resource = doc.entity('c2fcc1e3-c38f-44ad-a0cf-e5ea2a6585b5',
+                              {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource',
+                               'ont:Extension': 'json'})
+        get_streetlights = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAssociatedWith(get_streetlights, this_script)
+        doc.usage(get_streetlights, resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval'})
+
+        streetlights = doc.entity('dat:jdbrawn_slarbi#crime',
+                              {prov.model.PROV_LABEL: 'Boston Colleges', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(streetlights, this_script)
+        doc.wasGeneratedBy(streetlights, get_streetlights, endTime)
+        doc.wasDerivedFrom(streetlights, resource, get_streetlights, get_streetlights, get_streetlights)
+
+        repo.logout()
+
+        return doc
