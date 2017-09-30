f56e49f358b01a3068f6760c4b0af12905d3fa57
Eli Saracino <esaracin@bu.edu>
2017-09-30 12:03:30 -0400

    added prov for first script

--- esaracin/boston_gov_extraction.py
+++ esaracin/boston_gov_extraction.py
@@ -37,7 +37,6 @@ class boston_gov_extraction(dml.Algorithm):
         repo['esaracin.crime_incidents'].insert_many(r)
         repo['esaracin.crime_incidents'].metadata({'complete':True})
 
-        print('half way there!')
 
 
         # Do the same as above for our gun dataset
@@ -59,6 +58,49 @@ class boston_gov_extraction(dml.Algorithm):
 
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
-        return 
+        '''Creates the provenance document describing the collection of data
+        occuring within this script.'''
+        
+        # Set up the database connection
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('esaracin', 'esaracin')
+
+        # Add useful namespaces for this prov doc
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')
+        doc.add_namespace('ont', 'http://datamechanics/io/ontology/')
+        doc.add_namespace('log', 'http://datamechanics.io/log/')
+        doc.add_namespace('gov', 'https://data.boston.gov/dataset') # Namespace specific to this script
+
+        # Add this script as a provenance agent to our document
+        this_script = doc.agent('alg:esaracin#boston_gov_extraction', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('gov:6220d948-eae2-4e4b-8723-2dc8e67722a3/resource/12cb3883-56f5-47de-afa5-3b1cf61b257b/',
+                   {'prov:label':'311, Service Requests',
+                    prov.model.PROV_TYPE:'ont:DataResource','ont:Extension':'csv'})
+        get_crime = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_guns = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAssociatedWith(get_crime, this_script)
+        doc.wasAssociatedWith(get_guns, this_script)
+        doc.usage(get_crime, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        doc.usage(get_guns, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+
+
+        crimes = doc.entity('dat:esaracin#crime_incidents', {prov.model.PROV_LABEL:'Crimes Reported', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(crimes, this_script)
+        doc.wasGeneratedBy(crimes, get_crime, endTime)
+        doc.wasDerivedFrom(crimes, resource, get_crime, get_crime, get_crime)
+
+        guns = doc.entity('dat:esaracin#gun_data', {prov.model.PROV_LABEL:'Guns Collected',prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(guns, this_script)
+        doc.wasGeneratedBy(guns, get_guns, endTime)
+        doc.wasDerivedFrom(guns, resource, get_guns, get_guns, get_guns)
+
+        repo.logout()
+
+
+        return doc
 
 boston_gov_extraction.execute()
+boston_gov_extraction.provenance()
