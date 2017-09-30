bf64d44081e0ff567c12e7319f5cd2580bc91ae5
Young Jun Choi <yjunchoi93@gmail.com>
2017-09-30 13:53:40 -0400

    Update example.py
    Update example.py

--- yjunchoi_yzhang71/example.py
+++ yjunchoi_yzhang71/example.py
@@ -43,7 +43,7 @@ class example(dml.Algorithm):
         endTime = datetime.datetime.now()
 
         return {"start":startTime, "end":endTime}
-    
+
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
         '''
@@ -90,7 +90,7 @@ class example(dml.Algorithm):
         doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
 
         repo.logout()
-                  
+
         return doc
 
 example.execute()
