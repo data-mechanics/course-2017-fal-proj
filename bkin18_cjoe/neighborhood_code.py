6ef3d3e94b587885c75f7c26727a9b74ed4708c1
Chris Joe <cjoe@bu.edu>
2017-10-01 17:08:51 -0400

    added neighborhood code

--- bkin18_cjoe/neighborhood_code.py
+++ bkin18_cjoe/neighborhood_code.py
@@ -7,10 +7,10 @@ import uuid
 import pdb
 import csv
 
-class snow_emergency_routes(dml.Algorithm):
+class neighborhood_code(dml.Algorithm):
     contributor = 'bkin18_cjoe'
     reads = []
-    writes = ['bkin18_cjoe.emergency_routes'] 
+    writes = ['bkin18_cjoe.neighborhoods'] 
 
     # Set up the database connection.
     client = dml.pymongo.MongoClient()
@@ -27,15 +27,15 @@ class snow_emergency_routes(dml.Algorithm):
         repo.authenticate('bkin18_cjoe', 'bkin18_cjoe') # should probably move this to auth
 
 
-        # Add Snow Emergency Routes
-        url = 'http://bostonopendata-boston.opendata.arcgis.com/datasets/4f3e4492e36f4907bcd307b131afe4a5_0.geojson'
+         # Add Neighborhoods
+        url = 'http://bostonopendata-boston.opendata.arcgis.com/datasets/3525b0ee6e6b427f9aab5d0a1d0a1a28_0.geojson'
         response = urllib.request.urlopen(url).read().decode("utf-8")
         r = json.loads(response)
         s = json.dumps(r, sort_keys=True, indent=2)
 
-        repo.dropCollection("emergency_routes")
-        repo.createCollection("emergency_routes")
-        repo['bkin18_cjoe.emergency_routes'].insert_many(r['features'])
+        repo.dropCollection("neighborhoods")
+        repo.createCollection("neighborhoods")
+        repo['bkin18_cjoe.neighborhoods'].insert_many(r['features'])
 
         endTime = datetime.datetime.now()
 
@@ -62,29 +62,25 @@ class snow_emergency_routes(dml.Algorithm):
         doc.add_namespace('bdp', 'http://bostonopendata-boston.opendata.arcgis.com/datasets/')
 
 
-        this_script = doc.agent('alg:bkin18_cjoe#snow_emergency_routes', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        this_script = doc.agent('alg:bkin18_cjoe#neighborhoods', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         
-        ## I don't actually know what the key is to this (bdp:wc8w-nujj)
-        # resource = doc.entity('bdp:wc8w-nujj', { 'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
-        resource = doc.entity('bdp:4f3e4492e36f4907bcd307b131afe4a5_0',
+        resource = doc.entity('bdp:3525b0ee6e6b427f9aab5d0a1d0a1a28_0.',
             {'prov:label':'311, Service Requests',
-            prov.model.PROV_TYPE:'ont:DataResource', 'bdp:Extension':'geojson'}) 
-
+            prov.model.PROV_TYPE:'ont:DataResource', 'bdp:Extension':'geojson'})
         ## Work on this later
         this_run = doc.activity('log:a'+str(uuid.uuid4()), startTime, endTime, { prov.model.PROV_TYPE:'ont:Retrieval'})#, 'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'})
 
-        route_activity = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        neighborhood_activity = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
 
-        routes = doc.entity('dat:bkin18_cjoe#emergency_routes', {prov.model.PROV_LABEL:'Snow Emergency Routes', prov.model.PROV_TYPE:'ont:DataSet'})
+        neighborhoods = doc.entity('dat:bkin18_cjoe#neighborhoods', {prov.model.PROV_LABEL:'Neighborhoods', prov.model.PROV_TYPE:'ont:DataSet'})
     
-        doc.wasAssociatedWith(route_activity, this_script)
-        doc.usage(route_activity, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        doc.wasAssociatedWith(neighborhood_activity, this_script)
+        doc.usage(neighborhood_activity, resource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
         # doc.wasAttributedTo(found, this_script)
 
-        doc.wasAttributedTo(routes, this_script)
-        doc.wasGeneratedBy(routes, route_activity, endTime)
-        doc.wasDerivedFrom(routes, resource, this_run, this_run, this_run)
-
+        doc.wasAttributedTo(neighborhoods, this_script)
+        doc.wasGeneratedBy(neighborhoods, neighborhood_activity, endTime)
+        doc.wasDerivedFrom(neighborhoods, resource, this_run, this_run, this_run)
 
         repo.logout()
 
@@ -92,8 +88,8 @@ class snow_emergency_routes(dml.Algorithm):
         return doc
 
 
-snow_emergency_routes.execute()
-doc = snow_emergency_routes.provenance()
+neighborhood_code.execute()
+doc = neighborhood_code.provenance()
 print(doc.get_provn())
 print(json.dumps(json.loads(doc.serialize()), indent=4))
 
