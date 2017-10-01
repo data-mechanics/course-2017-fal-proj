c6632327957d1e739e56eba496a6c101e18d9432
Young Jun Choi <yjunchoi93@gmail.com>
2017-10-01 00:17:45 -0400

    Update averageDelay.py
    Please check line 60.

--- yjunchoi_yzhang71/averageDelay.py
+++ yjunchoi_yzhang71/averageDelay.py
@@ -54,11 +54,10 @@ class averageDelay(dml.Algorithm):
         doc.add_namespace('dat', 'http://datamechanics.io/data/yjunchoi_yzhang71') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
         doc.add_namespace('mbtaviz', 'https://github.com/yzhang71/mbtaviz.github.io/blob/master/data/')
 
         this_script = doc.agent('alg:yjunchoi_yzhang71#averageDelay', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        resource = doc.entity('mbtaviz:average-actual-delays', {'prov:label':'Average Actual Delay', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         get_averageDelay = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         doc.wasAssociatedWith(get_averageDelay, this_script)
         doc.usage(get_averageDelay, resource, startTime, None,
