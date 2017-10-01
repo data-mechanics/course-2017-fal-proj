9506f043f8158fe68d737e54efcf849665eba460
wht931011 <wht931011@gmail.com>
2017-10-01 19:15:34 -0400

    Finish

--- htw93_tscheung/getData.py
+++ htw93_tscheung/getData.py
@@ -12,7 +12,7 @@ class getData(dml.Algorithm):
     reads = []
     writes = ['htw93_tscheung.BostonCrime', 'htw93_tscheung.NewYorkCityCrime',
               'htw93_tscheung.BostonSchools', 'htw93_tscheung.NewYorkCitySchools',
-              'htw93_tscheung.BostonRestaurants', 'htw93_tscheung.NewYorkCityRestaurants']
+              'htw93_tscheung.BostonHospitals', 'htw93_tscheung.NewYorkCityHospitals']
 
     @staticmethod
     def execute(trial = False):
@@ -62,6 +62,26 @@ class getData(dml.Algorithm):
         repo['htw93_tscheung.NewYorkCitySchools'].insert_many(r)
         #repo['htw93_tscheung.BostonCrime'].metadata({'complete':True})
         print('Finished rectrieving htw93_tscheung.NewYorkCitySchools')
+        
+        url = 'https://data.cityofboston.gov/resource/46f7-2snz.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        r = json.loads(response)
+        s = json.dumps(r, sort_keys=True, indent=2)
+        repo.dropCollection("BostonHospitals")
+        repo.createCollection("BostonHospitals")
+        repo['htw93_tscheung.BostonHospitals'].insert_many(r)
+        #repo['htw93_tscheung.BostonCrime'].metadata({'complete':True})
+        print('Finished rectrieving htw93_tscheung.BostonHospitals')
+        
+        url = 'https://data.cityofnewyork.us/resource/ymhw-9cz9.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        r = json.loads(response)
+        s = json.dumps(r, sort_keys=True, indent=2)
+        repo.dropCollection("NewYorkCityHospitals")
+        repo.createCollection("NewYorkCityHospitals")
+        repo['htw93_tscheung.NewYorkCityHospitals'].insert_many(r)
+        #repo['htw93_tscheung.BostonCrime'].metadata({'complete':True})
+        print('Finished rectrieving htw93_tscheung.NewYorkCityHospitals')
 
         repo.logout()
 
@@ -86,33 +106,69 @@ class getData(dml.Algorithm):
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+        doc.add_namespace('ndp', 'https://data.cityofnewyork.us/resource/')
+        doc.add_namespace('bod', 'http://bostonopendata-boston.opendata.arcgis.com/datasets/')
 
-        this_script = doc.agent('alg:alice_bob#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_found, this_script)
-        doc.wasAssociatedWith(get_lost, this_script)
-        doc.usage(get_found, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
-                  }
-                  )
-        doc.usage(get_lost, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
-                  }
-                  )
+        getData_script = doc.agent('alg:htw93_tscheung#getData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        
+        BostonSchoolsResource = doc.entity('bod:cbf14bb032ef4bd38e20429f71acb61a_2', {'prov:label':'Boston Schools', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
+        NewYorkCitySchoolsResource = doc.entity('ndp:8pnn-kkif', {'prov:label':'New York City Schools', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        BostonCrimeResource = doc.entity('bdp:29yf-ye7nf', {'prov:label':'Boston Crime', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        NewYorkCityCrimeResource = doc.entity('ndp:qgea-i56i', {'prov:label':'New York City Crime', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        BostonHospitalsResource = doc.entity('bdp:46f7-2snz', {'prov:label':'Boston Hospitals', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        NewYorkCityHospitalsResource = doc.entity('ndp:ymhw-9cz9', {'prov:label':'New York City Hospitals', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
 
-        lost = doc.entity('dat:alice_bob#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(lost, this_script)
-        doc.wasGeneratedBy(lost, get_lost, endTime)
-        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+        get_BostonSchools = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_NewYorkCitySchools = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_BostonCrime = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_NewYorkCityCrime = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_BostonHospitals = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_NewYorkCityHospitals = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        
+        doc.wasAssociatedWith(get_BostonSchools, getData_script)
+        doc.wasAssociatedWith(get_NewYorkCitySchools, getData_script)
+        doc.wasAssociatedWith(get_BostonCrime, getData_script)
+        doc.wasAssociatedWith(get_NewYorkCityCrime, getData_script)
+        doc.wasAssociatedWith(get_BostonHospitals, getData_script)
+        doc.wasAssociatedWith(get_NewYorkCityHospitals, getData_script)
+        
+        doc.usage(get_BostonSchools, BostonSchoolsResource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        doc.usage(get_NewYorkCitySchools, NewYorkCitySchoolsResource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        doc.usage(get_BostonCrime, BostonCrimeResource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        doc.usage(get_NewYorkCityCrime, NewYorkCityCrimeResource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        doc.usage(get_BostonHospitals, BostonHospitalsResource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        doc.usage(get_NewYorkCityHospitals, NewYorkCityHospitalsResource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
 
-        found = doc.entity('dat:alice_bob#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(found, this_script)
-        doc.wasGeneratedBy(found, get_found, endTime)
-        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+
+        bostonSchools = doc.entity('dat:htw93_tscheung#BostonSchools', {prov.model.PROV_LABEL:'Boston Schools', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(bostonSchools, getData_script)
+        doc.wasGeneratedBy(bostonSchools, get_BostonSchools, endTime)
+        doc.wasDerivedFrom(bostonSchools, BostonSchoolsResource, get_BostonSchools, get_BostonSchools, get_BostonSchools)
+
+        newYorkCitySchools = doc.entity('dat:htw93_tscheung#NewYorkCitySchools', {prov.model.PROV_LABEL:'New York City Schools', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(newYorkCitySchools, getData_script)
+        doc.wasGeneratedBy(newYorkCitySchools, get_NewYorkCitySchools, endTime)
+        doc.wasDerivedFrom(newYorkCitySchools, NewYorkCitySchoolsResource, get_NewYorkCitySchools, get_NewYorkCitySchools, get_NewYorkCitySchools)
+        
+        bostonCrime = doc.entity('dat:htw93_tscheung#BostonCrime', {prov.model.PROV_LABEL:'Boston Crime', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(bostonCrime, getData_script)
+        doc.wasGeneratedBy(bostonCrime, get_BostonCrime, endTime)
+        doc.wasDerivedFrom(bostonCrime, BostonCrimeResource, get_BostonCrime, get_BostonCrime, get_BostonCrime)
+        
+        newYorkCityCrime = doc.entity('dat:htw93_tscheung#NewYorkCityCrime', {prov.model.PROV_LABEL:'New York City Crime', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(newYorkCityCrime, getData_script)
+        doc.wasGeneratedBy(newYorkCityCrime, get_NewYorkCitySchools, endTime)
+        doc.wasDerivedFrom(newYorkCityCrime, NewYorkCityCrimeResource, get_NewYorkCityCrime, get_NewYorkCityCrime, get_NewYorkCityCrime)
+        
+        bostonHospitals = doc.entity('dat:htw93_tscheung#BostonHospitals', {prov.model.PROV_LABEL:'Boston Hospitals', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(bostonHospitals, getData_script)
+        doc.wasGeneratedBy(bostonHospitals, get_BostonHospitals, endTime)
+        doc.wasDerivedFrom(bostonHospitals, BostonHospitalsResource, get_BostonHospitals, get_BostonHospitals, get_BostonHospitals)
+        
+        newYorkCityHospitals = doc.entity('dat:htw93_tscheung#NewYorkCityHospitals', {prov.model.PROV_LABEL:'New York City Hospitals', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(newYorkCityHospitals, getData_script)
+        doc.wasGeneratedBy(newYorkCityHospitals, get_NewYorkCityHospitals, endTime)
+        doc.wasDerivedFrom(newYorkCityHospitals, NewYorkCityHospitalsResource, get_NewYorkCityHospitals, get_NewYorkCityHospitals, get_NewYorkCityHospitals)
 
         repo.logout()
                   
