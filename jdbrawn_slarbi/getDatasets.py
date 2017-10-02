644367802264a1d7d0e08d1d8a65da8d63dec24c
David Brawn <david.brawn@gmail.com>
2017-10-02 00:45:15 -0400

    updated provenonce

--- jdbrawn_slarbi/getDatasets.py
+++ jdbrawn_slarbi/getDatasets.py
@@ -4,7 +4,6 @@ import dml
 import prov.model
 import datetime
 import uuid
-import pandas as pd
 
 class getData(dml.Algorithm):
     contributor = 'jdbrawn_slarbi'
@@ -40,10 +39,10 @@ class getData(dml.Algorithm):
         repo['jdbrawn_slarbi.crime'].insert_many(r['result']['records'])
 
         # Get crash data
-        url = 'http://datamechanics.io/data/jdbrawn_slarbi/CrashData.csv'
-        csv_data = pd.read_csv(url)
-        json_data = csv_data.to_json(orient='records')
-        r = json.loads(json_data)
+        url = 'http://datamechanics.io/data/jdbrawn_slarbi/CarCrashData.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        r = json.loads(response)
+        s = json.dumps(r, sort_keys=True, indent=2)
         repo.dropCollection("crash")
         repo.createCollection("crash")
         repo['jdbrawn_slarbi.crash'].insert_many(r)
@@ -68,39 +67,43 @@ class getData(dml.Algorithm):
         repo.authenticate('jdbrawn_slarbi', 'jdbrawn_slarbi')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
-        doc.add_namespace('ont',
-                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
-        doc.add_namespace('bdp', 'https://data.boston.gov/datastore/odata3.0/')
+        doc.add_namespace('bdp', 'https://data.boston.gov/api/action/datastore_search?resource_id=')
+        doc.add_namespace('car', 'http://datamechanics.io/data/jdbrawn_slarbi/')
 
-        this_script = doc.agent('alg:jdbrawn_slarbi#getData',
-                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        this_script = doc.agent('alg:jdbrawn_slarbi#getData', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
 
-        resource_colleges = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        resource_crime = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        resource_colleges = doc.entity('bdp:208dc980-a278-49e3-b95b-e193bb7bb6e4', {'prov:label':'Boston Universities and Colleges', prov.model.PROV_TYPE:'ont:DataResource'})
+        resource_crime = doc.entity('bdp:12cb3883-56f5-47de-afa5-3b1cf61b257b&limit=70', {'prov:label':'Boston Crime', prov.model.PROV_TYPE:'ont:DataResource'})
+        resource_crashes = doc.entity('car:CarCrashData', {'prov:label':'Boston Crashes', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
 
         get_colleges = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
         get_crime = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        get_crashes = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
 
         doc.wasAssociatedWith(get_colleges, this_script)
         doc.wasAssociatedWith(get_crime, this_script)
+        doc.wasAssociatedWith(get_crashes, this_script)
 
-        doc.usage(get_colleges, resource_colleges, startTime, None,
-                  {prov.model.PROV_TYPE: 'ont:Retrieval'})
-        doc.usage(get_crime, resource_crime, startTime, None,
-                  {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        doc.usage(get_colleges, resource_colleges, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        doc.usage(get_crime, resource_crime, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        doc.usage(get_crashes, resource_crashes, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
 
-        colleges = doc.entity('dat:jdbrawn_slarbi#crime',
-                           {prov.model.PROV_LABEL: 'Boston Universities and Colleges', prov.model.PROV_TYPE: 'ont:DataSet'})
+        colleges = doc.entity('dat:jdbrawn_slarbi#colleges', {prov.model.PROV_LABEL: 'Boston Universities and Colleges', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(colleges, this_script)
         doc.wasGeneratedBy(colleges, get_colleges, endTime)
         doc.wasDerivedFrom(colleges, resource_colleges, get_colleges, get_colleges, get_colleges)
 
-        crime = doc.entity('dat:jdbrawn_slarbi#crime',
-                           {prov.model.PROV_LABEL: 'Boston Crime', prov.model.PROV_TYPE: 'ont:DataSet'})
+        crime = doc.entity('dat:jdbrawn_slarbi#crime', {prov.model.PROV_LABEL: 'Boston Crime', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(crime, this_script)
         doc.wasGeneratedBy(crime, get_crime, endTime)
         doc.wasDerivedFrom(crime, resource_crime, get_crime, get_crime, get_crime)
+
+        crash = doc.entity('dat:jdbrawn_slarbi#crashes', {prov.model.PROV_LABEL: 'Boston Crashes', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(crash, this_script)
+        doc.wasGeneratedBy(crash, get_crashes, endTime)
+        doc.wasDerivedFrom(crash, resource_crashes, get_crashes, get_crashes, get_crashes)
 
         repo.logout()
 
