b87892d01c9fcf193042d3bc6a5b319ca40dab3c
Adriana D'Souza <adsouza@bu.edu>
2017-10-01 22:20:38 -0400

    added Crime Request data

---
+++ adsouza_mcsmocha/requestCrimeData.py
@@ -0,0 +1,81 @@
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+
+class requestCrimeData(dml.Algorithm):
+    contributor = 'adsouza_mcsmocha'
+    reads = []
+    writes = ['adsouza_mcsmocha.CrimeData']
+
+    @staticmethod
+    def execute(trial = False):
+        '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('adsouza_mcsmocha', 'adsouza_mcsmocha')
+
+        url = 'https://data.boston.gov/export/12c/b38/12cb3883-56f5-47de-afa5-3b1cf61b257b.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        r = json.loads(response)
+        s = json.dumps(r, sort_keys=True, indent=2)
+        repo.dropCollection("CrimeData")
+        repo.createCollection("CrimeData")
+        repo['adsouza_mcsmocha.CrimeData'].insert_many(r)
+        repo['adsouza_mcsmocha.CrimeData'].metadata({'complete':True})
+        print(repo['adsouza_mcsmocha.CrimeData'].metadata())
+
+        repo.logout()
+
+        endTime = datetime.datetime.now()
+
+        return {"start": startTime, "end": endTime}
+
+    @staticmethod
+    def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        '''
+            Create the provenance document describing everything happening
+            in this script. Each run of the script will generate a new
+            document describing that invocation event.
+            '''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('adsouza_mcsmocha', 'adsouza_mcsmocha')
+       	doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        
+        # Additional resource
+        doc.add_namespace('anb', 'https://data.boston.gov/')
+
+        this_script = doc.agent('alg:adsouza_mcsmocha#requestCrimeData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('anb:12cb3883-56f5-47de-afa5-3b1cf61b257b', {'prov:label':'Crime Data, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        get_crime = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_crime, this_script)
+        doc.usage(get_crime, resource, startTime, None,
+        	{prov.model.PROV_TYPE:'ont:Retrieval',
+        	'ont:Query':'?type=Crime+Data&$INCIDENT_NUMBER,OFFENSE_CODE,OFFENSE_CODE_GROUP,OFFENSE_DESCRIPTION,DISTRICT,REPORTING_AREA,SHOOTING,OCCURRED_ON_DATE,YEAR,MONTH,NAY_OF_WEEK,HOUR,UCR_PART,STREET,Lat,Long,Location'
+        	}
+        	)
+
+        crime = doc.entity('dat:adsouza_mcsmocha#CrimeData', {prov.model.PROV_LABEL:'Crime Data', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(crime, this_script)
+        doc.wasGeneratedBy(crime, get_crime, endTime)
+        doc.wasDerivedFrom(crime, resource, get_crime, get_crime, get_crime)
+
+        repo.logout()
+
+        return doc
+
+requestCrimeData.execute()
+doc = requestCrimeData.provenance()
+print(doc.get_provn())
+print(json.dumps(json.loads(doc.serialize()), indent=4))
