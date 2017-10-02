a98813ebf2a4731f39f5328ed1cc5d473a5ee908
Adriana D'Souza <adsouza@bu.edu>
2017-10-02 11:31:12 -0400

    deleted planning districts; added city score file

--- adsouza_mcsmocha/requestCityScore.py
+++ adsouza_mcsmocha/requestCityScore.py
@@ -5,10 +5,10 @@ import prov.model
 import datetime
 import uuid
 
-class requestPlanningDistricts(dml.Algorithm):
+class requestCityScore(dml.Algorithm):
     contributor = 'adsouza_mcsmocha'
     reads = []
-    writes = ['adsouza_mcsmocha.PlanningDistricts']
+    writes = ['adsouza_mcsmocha.CityScore']
 
     @staticmethod
     def execute(trial = False):
@@ -20,15 +20,15 @@ class requestPlanningDistricts(dml.Algorithm):
         repo = client.repo
         repo.authenticate('adsouza_mcsmocha', 'adsouza_mcsmocha')
 
-        url = 'https://data.opendatasoft.com/api/records/1.0/search/?dataset=planning-districts%40boston'
+        url = 'https://data.boston.gov/export/5bc/e8e/5bce8e71-5192-48c0-ab13-8faff8fef4d7.json'
         response = urllib.request.urlopen(url).read().decode("utf-8")
         r = json.loads(response)
         s = json.dumps(r, sort_keys=True, indent=2)
-        repo.dropCollection("PlanningDistricts")
-        repo.createCollection("PlanningDistricts")
-        repo['adsouza_mcsmocha.PlanningDistricts'].insert_many(r)
-        repo['adsouza_mcsmocha.PlanningDistricts'].metadata({'complete':True})
-        print(repo['adsouza_mcsmocha.PlanningDistricts'].metadata())
+        repo.dropCollection("CityScore")
+        repo.createCollection("CityScore")
+        repo['adsouza_mcsmocha.CityScore'].insert_many(r)
+        repo['adsouza_mcsmocha.CityScore'].metadata({'complete':True})
+        print(repo['adsouza_mcsmocha.CityScore'].metadata())
 
         repo.logout()
 
@@ -54,28 +54,28 @@ class requestPlanningDistricts(dml.Algorithm):
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         
         # Additional resource
-        doc.add_namespace('ods', 'https://data.opendatasoft.com/')
+        doc.add_namespace('anb', 'https://data.boston.gov/')
 
-        this_script = doc.agent('alg:adsouza_mcsmocha#requestPlanningDistricts', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('anb:planning-districts@boston', {'prov:label':'Planning Districts, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_plandist = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_plandist, this_script)
-        doc.usage(get_plandist, resource, startTime, None,
+        this_script = doc.agent('alg:adsouza_mcsmocha#requestCityScore', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('anb:5bce8e71-5192-48c0-ab13-8faff8fef4d7', {'prov:label':'CityScore, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        get_city = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_city, this_script)
+        doc.usage(get_city, resource, startTime, None,
         	{prov.model.PROV_TYPE:'ont:Retrieval',
-        	'ont:Query':'?type=Planning+Districts&$objectid,geo_point_2d,shapearea,pd,id'
+        	'ont:Query':'?type=CityScore&$CTY_SCR_NAME,CTY_SCR_NBR_QT_01,CTY_SCR_TGT_01'
         	}
         	)
 
-        plandist = doc.entity('dat:adsouza_mcsmocha#PlanningDistricts', {prov.model.PROV_LABEL:'Planning Districts', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(plandist, this_script)
-        doc.wasGeneratedBy(plandist, get_plandist, endTime)
-        doc.wasDerivedFrom(plandist, resource, get_plandist, get_plandist, get_plandist)
+        city_score = doc.entity('dat:adsouza_mcsmocha#CityScore', {prov.model.PROV_LABEL:'CityScore', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(city_score, this_script)
+        doc.wasGeneratedBy(city_score, get_city, endTime)
+        doc.wasDerivedFrom(city_score, resource, get_city, get_city, get_city)
 
         repo.logout()
 
         return doc
 
-requestPlanningDistricts.execute()
-doc = requestPlanningDistricts.provenance()
+requestCityScore.execute()
+doc = requestCityScore.provenance()
 print(doc.get_provn())
 print(json.dumps(json.loads(doc.serialize()), indent=4))
