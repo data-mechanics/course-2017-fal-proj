deb2c5287d06e941b48edbbcb34593f4da9c4729
SLarbiBU <slarbi@bu.edu>
2017-10-02 21:40:27 -0400

    Update getDatasets.py

--- jdbrawn_slarbi/getDatasets.py
+++ jdbrawn_slarbi/getDatasets.py
@@ -56,6 +56,24 @@ class getDatasets(dml.Algorithm):
         repo.createCollection("mbta")
         repo['jdbrawn_slarbi.mbta'].insert_many(r)
 
+        #ENTERTAINMENT DATA
+        url = 'https://data.cityofboston.gov/resource/cz6t-w69j.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        r = json.loads(response)
+        s = json.dumps(r, sort_keys=True, indent=2)
+        repo.dropCollection("entertain")
+        repo.createCollection("entertain")
+        repo['jdbrawn_slarbi.entertain'].insert_many(r)
+
+        #FOOD LICENSE DATA
+        url = 'https://data.cityofboston.gov/resource/fdxy-gydq.json'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        a = json.loads(response)
+        b = json.dumps(a, sort_keys=True, indent=2)
+        repo.dropCollection("food")
+        repo.createCollection("food")
+        repo['jdbrawn_slarbi.food'].insert_many(a)
+
         repo.logout()
 
         endTime = datetime.datetime.now()
@@ -81,6 +99,7 @@ class getDatasets(dml.Algorithm):
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
         doc.add_namespace('bdp', 'https://data.boston.gov/api/action/datastore_search?resource_id=')
         doc.add_namespace('591', 'http://datamechanics.io/data/jdbrawn_slarbi/')
+        doc.add_namespace('bdp1', 'https://data.cityofboston.gov/resource/')
 
         this_script = doc.agent('alg:jdbrawn_slarbi#getData', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
 
@@ -88,21 +107,32 @@ class getDatasets(dml.Algorithm):
         resource_crime = doc.entity('bdp:12cb3883-56f5-47de-afa5-3b1cf61b257b&limit=10000', {'prov:label':'Boston Crime', prov.model.PROV_TYPE:'ont:DataResource'})
         resource_crashes = doc.entity('591:CarCrashData', {'prov:label':'Boston Crashes', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         resource_mbta = doc.entity('591:MBTA_Bus_Stops', {'prov:label':'MBTA Bus Stops', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
+        resource_entertain = doc.entity('bdp1:cz6t-w69j', {'prov:label':'Entertainment Data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        resource_food = doc.entity('bdp1:fdxy-gydq', {'prov:label':'Food License Data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
 
+        
         get_colleges = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
         get_crime = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
         get_crashes = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
         get_mbta = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        get_entertainment_data = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_food_license = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+
 
         doc.wasAssociatedWith(get_colleges, this_script)
         doc.wasAssociatedWith(get_crime, this_script)
         doc.wasAssociatedWith(get_crashes, this_script)
         doc.wasAssociatedWith(get_mbta, this_script)
+        doc.wasAssociatedWith(get_entertainment_data, this_script)
+        doc.wasAssociatedWith(get_food_license, this_script)
+
 
         doc.usage(get_colleges, resource_colleges, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
         doc.usage(get_crime, resource_crime, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
         doc.usage(get_crashes, resource_crashes, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
         doc.usage(get_mbta, resource_mbta, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        doc.usage(get_entertainment_data, resource_entertain, startTime, None,{prov.model.PROV_TYPE:'ont:Retrieval',})
+        doc.usage(get_food_license, resource_food, startTime, None,{prov.model.PROV_TYPE:'ont:Retrieval',})
 
         colleges = doc.entity('dat:jdbrawn_slarbi#colleges', {prov.model.PROV_LABEL: 'Boston Universities and Colleges', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(colleges, this_script)
@@ -123,6 +153,16 @@ class getDatasets(dml.Algorithm):
         doc.wasAttributedTo(mbta, this_script)
         doc.wasGeneratedBy(mbta, get_mbta, endTime)
         doc.wasDerivedFrom(mbta, resource_mbta, get_mbta, get_mbta, get_mbta)
+
+        entertainment_data = doc.entity('dat:jdbrawn_slarbi#entertain', {prov.model.PROV_LABEL:'Entertainment Data', prov.model.PROV_TYPE:'ont:DataSet', 'ont:Extension':'json'})
+        doc.wasAttributedTo(entertainment_data, this_script)
+        doc.wasGeneratedBy(entertainment_data, get_entertainment_data, endTime)
+        doc.wasDerivedFrom(entertainment_data, resource_entertain, get_entertainment_data, get_entertainment_data, get_entertainment_data)
+
+        food_license = doc.entity('dat:jdbrawn_slarbi#food', {prov.model.PROV_LABEL:'Food License Data', prov.model.PROV_TYPE:'ont:DataSet', 'ont:Extension':'json'})
+        doc.wasAttributedTo(food_license, this_script)
+        doc.wasGeneratedBy(food_license, get_food_license, endTime)
+        doc.wasDerivedFrom(food_license, resource_food, get_food_license, get_food_license, get_food_license)
 
         repo.logout()
 
