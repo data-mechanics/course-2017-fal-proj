a3d74245d27403569d3757bb68cbffdfac983872
Jed Caluag <Jed@dhcp-wifi-8021x-168-122-209-79.bu.edu>
2017-10-03 18:06:47 -0400

    new file

---
+++ alanbur_jcaluag/bikeNetwork.py
@@ -0,0 +1,51 @@
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+
+class bikeNetwork():
+    # contributor = 'test'
+    # reads = []
+    # writes = ['test.trafficSignal']
+    # contributor = 'alice_bob'
+    # reads = []
+    # writes = ['alice_bob.lost', 'alice_bob.found']
+
+    @staticmethod
+    def execute(trial = False):
+        '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('alanbur_jcaluag', 'alanbur_jcaluag')
+
+        url = 'http://bostonopendata-boston.opendata.arcgis.com/datasets/d02c9d2003af455fbc37f550cc53d3a4_0.geojson'
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        r = json.loads(response)
+        features=r['features']
+
+        features = [
+
+            [{'Location':dict['properties']['STREET_NAM']},
+             {'Coordinates': dict['geometry']['coordinates']}
+
+             ] for dict in features
+        ]
+
+        for x in features:
+            print(x,'\n')
+        repo.dropCollection("trafficSignal")
+        repo.createCollection("trafficSignal")
+        repo['alanbur_jcaluag.trafficSignal'].insert_many(features)
+        repo['alanbur_jcaluag.trafficSignal'].metadata({'complete':True})
+        repo.logout()
+
+        endTime = datetime.datetime.now()
+
+        return {"start":startTime, "end":endTime}
+
+bikeNetwork.execute()
