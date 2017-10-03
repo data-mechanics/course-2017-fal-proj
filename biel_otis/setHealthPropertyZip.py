92e2ed87f9241737212cbff9af1c04782ed0fe0b
James Otis <jamesotis@dhcp-wifi-8021x-155-41-48-23.bu.edu>
2017-10-03 18:56:31 -0400

    Changed a bunch of files

--- biel_otis/setHealthPropertyZip.py
+++ biel_otis/setHealthPropertyZip.py
@@ -6,7 +6,76 @@ import datetime
 import uuid
 import time
 import ssl
-from biel_otis.HelperFunctions.helperFunctions import *
+#Uses Helpers
+from math import sin, cos, sqrt, atan2, radians
+# approximate radius of earth in km
+
+def union(R, S):
+    return R + S
+
+def difference(R, S):
+    return [t for t in R if t not in S]
+
+def intersect(R, S):
+    return [t for t in R if t in S]
+
+def project(R, p):
+    return [p(t) for t in R]
+
+def select(R, s):
+    return [t for t in R if s(t)]
+ 
+def product(R, S):
+    return [(t,u) for t in R for u in S]
+
+def aggregate(R, f):
+    keys = {r[0] for r in R}
+    return [(key, f([v for (k,v) in R if k == key])) for key in keys]
+
+
+def calculateDist(d1, d2):
+    R = 6373.0
+    d1 = d1.replace("(", "").replace(")", "")
+    d1 = d1.split(",")
+    d1 = (float(d1[0]), float(d1[1]))
+
+    d2 = d2.replace("(", "").replace(")", "")
+    d2 = d2.split(",")
+    d2 = (float(d2[0]), float(d2[1]))
+
+    lat1 = radians(d1[0])
+    lon1 = radians(d1[1])
+    lat2 = radians(d2[0])
+    lon2 = radians(d2[1])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance <= 1
+
+
+def dist(p, q):
+    (x1,y1) = p
+    (x2,y2) = q
+    return (x1-x2)**2 + (y1-y2)**2
+
+def plus(args):
+    p = [0,0]
+    for (x,y) in args:
+        p[0] += x
+        p[1] += y
+    return tuple(p)
+
+def scale(p, c):
+    (x,y) = p
+    return (x/c, y/c)
+#End Helpers
+
+
 
 class setHealthPropertyZip(dml.Algorithm):
     contributor = 'biel_otis'
