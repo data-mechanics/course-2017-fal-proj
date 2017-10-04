4e91a9cded4f881575a770dd5c0008630f5858dd
Andrew I Quan <aquan@bu.edu>
2017-10-04 17:19:47 -0400

    Add files via upload
    Finished Provenance

--- aquan_erj826/gunsRecovered.py
+++ aquan_erj826/gunsRecovered.py
@@ -25,8 +25,8 @@ import uuid
 
 class gunsRecovered(dml.Algorithm):
     contributor = 'erj826'
-    reads = []
-    writes = ['aquan_erj826.gunsRecovered']
+    reads = ['repo.aquan_erj826.firearms']
+    writes = ['aquan_erj826.gunsRecoveredNums']
 
     @staticmethod
     def execute(trial = False):
@@ -71,47 +71,41 @@ class gunsRecovered(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
+        repo.authenticate('erj826', 'erj826')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        #resources:
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
-
-        this_script = doc.agent('alg:alice_bob#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_found, this_script)
-        doc.wasAssociatedWith(get_lost, this_script)
-        doc.usage(get_found, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
-                  }
-                  )
-        doc.usage(get_lost, resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
-                  }
+        doc.add_namespace('cdp', 'https://data.cambridgema.gov/resource/') 
+        doc.add_namespace('svm','https://data.somervillema.gov/resource/')
+        
+        #define the agent
+        this_script = doc.agent('alg:aquan_erj826#gunsRecovered', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        
+        #define the firearms.json that we are using
+        resource = doc.entity('dat:aquan_erj826#firearms', {'prov:label':'firearmsCollectionMongoDB', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        
+        #define the calculation that we did
+        get_summation = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_summation, this_script)
+        doc.usage(get_summation, resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Calculation'}
                   )
 
-        lost = doc.entity('dat:alice_bob#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(lost, this_script)
-        doc.wasGeneratedBy(lost, get_lost, endTime)
-        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
-
-        found = doc.entity('dat:alice_bob#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(found, this_script)
-        doc.wasGeneratedBy(found, get_found, endTime)
-        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+        gunsRecoveredNums = doc.entity('dat:aquan_erj826#gunsRecoveredNums', {prov.model.PROV_LABEL:'Guns Recovered List', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(gunsRecoveredNums, this_script)
+        doc.wasGeneratedBy(gunsRecoveredNums, get_summation, endTime)
+        doc.wasDerivedFrom(gunsRecoveredNums, resource, get_summation, get_summation,get_summation)
 
         repo.logout()
                   
         return doc
 
-gunsRecovered.execute()
-# doc = example.provenance()
-# print(doc.get_provn())
-# print(json.dumps(json.loads(doc.serialize()), indent=4))
+#gunsRecovered.execute()
+#doc = gunsRecovered.provenance()
+#print(doc.get_provn())
+#print(json.dumps(json.loads(doc.serialize()), indent=4))
 
 ## eof
