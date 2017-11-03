85b7233c69d9d00a37f787562886642521dc3805
Chris Joe <cjoe@bu.edu>
2017-11-02 20:35:37 -0400

    Fixed unfinished code and added agg sum

--- bkin18_cjoe_klovett_sbrz/retrievePropertyAssessmentData.py
+++ bkin18_cjoe_klovett_sbrz/retrievePropertyAssessmentData.py
@@ -7,9 +7,9 @@ import uuid
 
 
 class retrievePropertyAssessmentData(dml.Algorithm):
-    contributor = 'sbrz_nedg'
+    contributor = 'bkin18_cjoe_klovett_sbrz'
     reads = []
-    writes = ['sbrz_nedg.property_assessment']
+    writes = ['bkin18_cjoe_klovett_sbrz.property_assessment']
 
     @staticmethod
     def execute(trial=False):
@@ -19,11 +19,12 @@ class retrievePropertyAssessmentData(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('sbrz_nedg', 'sbrz_nedg')
+        repo.authenticate('bkin18_cjoe_klovett_sbrz', 'bkin18_cjoe_klovett_sbrz')
 
         # Property Assessment Data Set
         property_assessment_url = urllib.request.Request(
-            "https://data.boston.gov/api/action/datastore_search?resource_id=062fc6fa-b5ff-4270-86cf-202225e40858&limit=200000")
+            "https://data.boston.gov/api/action/datastore_search?resource_id=062fc6fa-b5ff-4270-86cf-202225e40858&limit=200")
+            ## I changed this to 200 because my laptop could not handle 200k datapoints - Chris
         property_assessment_response = urllib.request.urlopen(property_assessment_url).read().decode("utf-8")
         property_assessment_json = json_util.loads(property_assessment_response)
 
@@ -31,9 +32,9 @@ class retrievePropertyAssessmentData(dml.Algorithm):
 
         repo.dropCollection("property_assessment")
         repo.createCollection("property_assessment")
-        repo['sbrz_nedg.property_assessment'].insert_many(property_assessment_json)
-        repo['sbrz_nedg.property_assessment'].metadata({'complete': True})
-        print(repo['sbrz_nedg.property_assessment'].metadata())
+        repo['bkin18_cjoe_klovett_sbrz.property_assessment'].insert_many(property_assessment_json)
+        repo['bkin18_cjoe_klovett_sbrz.property_assessment'].metadata({'complete': True})
+        print(repo['bkin18_cjoe_klovett_sbrz.property_assessment'].metadata())
 
         repo.logout()
 
@@ -52,7 +53,7 @@ class retrievePropertyAssessmentData(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('sbrz_nedg', 'sbrz_nedg')
+        repo.authenticate('bkin18_cjoe_klovett_sbrz', 'bkin18_cjoe_klovett_sbrz')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont',
@@ -60,14 +61,14 @@ class retrievePropertyAssessmentData(dml.Algorithm):
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
         doc.add_namespace('bdp', 'https://data.boston.gov/api/action/')
 
-        this_script = doc.agent('alg:sbrz_nedg#retrievePropertyAssessmentData', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        this_script = doc.agent('alg:bkin18_cjoe_klovett_sbrz#retrievePropertyAssessmentData', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
         resource = doc.entity('bdp:062fc6fa-b5ff-4270-86cf-202225e40858', {'prov:label': 'Property Assessment FY2017', prov.model.PROV_TYPE: 'ont:DataResource', 'ont:Extension': 'json'})
         get_property_data = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
 
         doc.wasAssociatedWith(get_property_data, this_script)
         doc.usage(resource, startTime, None)
 
-        property_db = doc.entity('dat:sbrz_nedg#property_assessment', {prov.model.PROV_LABEL: 'property_assessment', prov.model.PROV_TYPE: 'ont:DataSet'})
+        property_db = doc.entity('dat:bkin18_cjoe_klovett_sbrz#property_assessment', {prov.model.PROV_LABEL: 'property_assessment', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(this_script, this_script)
         doc.wasGeneratedBy(get_property_data)
         doc.wasDerivedFrom(resource, property_db)
--- bkin18_cjoe_klovett_sbrz/selectAddresses.py
+++ bkin18_cjoe_klovett_sbrz/selectAddresses.py
@@ -30,11 +30,23 @@ class selectAddresses(dml.Algorithm):
             if address['PTYPE'] in property_lookup:
                 x.append(address)
 
+        print(x)
+
+        ## agg sum
+        street_agg = {}
+        for street in x:
+            try:
+                street_agg[street['ST_NAME']] = street_agg[street['ST_NAME']] + 1
+            except KeyError:
+                street_agg[street['ST_NAME']] = 1
+        print(street_agg)
+        ## TODO: add this to our collection 
+
         repo.dropCollection('bkin18_cjoe_klovett_sbrz.property_assessment_addresses')
         repo.createCollection('bkin18_cjoe_klovett_sbrz.property_assessment_addresses')
         repo['bkin18_cjoe_klovett_sbrz.property_assessment_addresses'].insert_many(x)
         repo['bkin18_cjoe_klovett_sbrz.property_assessment_addresses'].metadata({'complete': True})
-        repo.dropCollection('bkin18_cjoe_klovett_sbrz.property_assessment')
+        # repo.dropCollection('bkin18_cjoe_klovett_sbrz.property_assessment')
 
         repo.logout()
 
@@ -53,18 +65,18 @@ class selectAddresses(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('sbrz_nedg', 'sbrz_nedg')
+        repo.authenticate('bkin18_cjoe_klovett_sbrz', 'bkin18_cjoe_klovett_sbrz')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont',
                           'http://datamechanics.io/ontology#DataSet')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
 
-        this_script = doc.agent('alg:sbrz_nedg#selectAddressesColleges',
+        this_script = doc.agent('alg:bkin18_cjoe_klovett_sbrz#selectAddressesColleges',
                                 {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
-        property_address_db = doc.entity('dat:sbrz_nedg#property_assessment',
+        property_address_db = doc.entity('dat:bkin18_cjoe_klovett_sbrz#property_assessment',
             {'prov:label': 'property_assessment', prov.model.PROV_TYPE: 'ont:DataSet'})
-        address_db = doc.entity('dat:sbrz_nedg#property_assessment_addresses',
+        address_db = doc.entity('dat:bkin18_cjoe_klovett_sbrz#property_assessment_addresses',
                                 {'prov:label': 'property_assessment_addresses', prov.model.PROV_TYPE: 'ont:DataSet'})
         select_address_data = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
 
