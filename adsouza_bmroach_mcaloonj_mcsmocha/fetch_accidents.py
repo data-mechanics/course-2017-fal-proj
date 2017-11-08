9e3fbfb921f645535bfd214cd3bd6083d8fa36fc
agdsouza <adsouza@bu.edu>
2017-11-08 17:59:39 -0500

    added full auth

--- adsouza_bmroach_mcaloonj_mcsmocha/fetch_accidents.py
+++ adsouza_bmroach_mcaloonj_mcsmocha/fetch_accidents.py
@@ -7,9 +7,9 @@ import uuid
 import requests
 
 class fetch_accidents(dml.Algorithm):
-        contributor = 'mcaloonj'
+        contributor = 'adsouza_bmroach_mcaloonj_mcsmocha'
         reads = []
-        writes = ['mcaloonj.accidents']
+        writes = ['adsouza_bmroach_mcaloonj_mcsmocha.accidents']
 
         @staticmethod
         def execute(trial=False):
@@ -17,7 +17,7 @@ class fetch_accidents(dml.Algorithm):
             # Set up the database connection.
             client = dml.pymongo.MongoClient()
             repo = client.repo
-            repo.authenticate('mcaloonj', 'mcaloonj')
+            repo.authenticate('adsouza_bmroach_mcaloonj_mcsmocha', 'adsouza_bmroach_mcaloonj_mcsmocha')
 
             url = 'https://data.boston.gov/api/3/action/datastore_search?resource_id=12cb3883-56f5-47de-afa5-3b1cf61b257b&q=Motor%20Vehicle%20Accident%20Response&limit=50000'
 
@@ -30,8 +30,8 @@ class fetch_accidents(dml.Algorithm):
             repo.dropCollection("accidents")
             repo.createCollection("accidents")
 
-            repo['mcaloonj.accidents'].insert_many(r["result"]["records"])
-            repo['mcaloonj.accidents'].metadata({'complete':True})
+            repo['adsouza_bmroach_mcaloonj_mcsmocha.accidents'].insert_many(r["result"]["records"])
+            repo['adsouza_bmroach_mcaloonj_mcsmocha.accidents'].metadata({'complete':True})
             #print(repo['mcaloonj.accidents'].metadata())
 
             repo.logout()
@@ -44,7 +44,7 @@ class fetch_accidents(dml.Algorithm):
         def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
             client = dml.pymongo.MongoClient()
             repo = client.repo
-            repo.authenticate('mcaloonj','mcaloonj')
+            repo.authenticate('adsouza_bmroach_mcaloonj_mcsmocha','adsouza_bmroach_mcaloonj_mcsmocha')
 
             doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
             doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
@@ -52,7 +52,7 @@ class fetch_accidents(dml.Algorithm):
             doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
             doc.add_namespace('dbg','https://data.boston.gov/api/3/action/datastore_search')
 
-            this_script = doc.agent('alg:mcaloonj#fetch_accidents', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extenstion':'py'})
+            this_script = doc.agent('alg:adsouza_bmroach_mcaloonj_mcsmocha#fetch_accidents', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extenstion':'py'})
             resource = doc.entity('dbg:'+str(uuid.uuid4()), {'prov:label': 'Crime Incident Reports', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extenstion':'json'})
 
             get_accidents = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
@@ -65,7 +65,7 @@ class fetch_accidents(dml.Algorithm):
                       }
                       )
 
-            accidents = doc.entity('dat:mcaloonj#accidents', {prov.model.PROV_LABEL:'Accidents',prov.model.PROV_TYPE:'ont:DataSet'})
+            accidents = doc.entity('dat:adsouza_bmroach_mcaloonj_mcsmocha#accidents', {prov.model.PROV_LABEL:'Accidents',prov.model.PROV_TYPE:'ont:DataSet'})
             doc.wasAttributedTo(accidents, this_script)
             doc.wasGeneratedBy(accidents, get_accidents, endTime)
             doc.wasDerivedFrom(accidents, resource, get_accidents, get_accidents, get_accidents)
