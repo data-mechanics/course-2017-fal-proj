e3060cacfdadffd1c76ecc5186840acf7df6e0e4
Lin Chen <lc546@bu.edu>
2017-11-11 17:30:09 -0500

    get zipcode
    get the zipcode

--- lc546_jofranco/propetymap.py
+++ lc546_jofranco/propetymap.py
@@ -1,4 +1,3 @@
-
 import urllib.request
 import json
 import dml
@@ -10,33 +9,29 @@ class propety(dml.Algorithm):
     contributor = 'lc546_jofranco'
     reads = []
     writes = ['lc546_jofranco.propety']
-    # nov 11
 
     @staticmethod
     def execute(trial = False):
         startTime = datetime.datetime.now()
-    	# Set up the database connection.
+        # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate("lc546_jofranco", "lc546_jofranco")
         url = 'https://data.cityofboston.gov/resource/g5b5-xrwi.json'
         response = urllib.request.urlopen(url).read().decode("utf-8")
         r = json.loads(response)
-        longitude= r['LONGITUDE']
-        latitude = r['LATITUDE']
+        zipcode = r['zipcode']
 
         s = json.dumps(r, sort_keys= True, indent = 2)
         print(type(s))
         repo.dropCollection("propety")
         repo.createCollection("propety")
-        repo["lc546_jofranco.propety"].insert_many(stations)
+        repo["lc546_jofranco.propety"].insert_many(zipcode)
         repo["lc546_jofranco.propety"].metadata({'complete':True})
         print(repo["lc546_jofranco.propety"].metadata())
         repo.logout()
         endTime = datetime.datetime.now()
         return {"start":startTime, "end":endTime}
-
-
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
         client = dml.pymongo.MongoClient()
@@ -49,13 +44,13 @@ class propety(dml.Algorithm):
         doc.add_namespace('bdp', 'https://secure.thepropety.com/data/')
         this_script = doc.agent('alg:lc546_jofranco#propety', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         resource = doc.entity('bdp:xgbq-327x', {'prov:label':'propety', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        get_houses = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_LABEL:'propety', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAssociatedWith(get_houses, this_script)
-        doc.usage(get_houses, resource, startTime)
-        houseinfo = doc.entity('dat:lc546_jofranco#houseinfo', {prov.model.PROV_LABEL:'propety Bike info', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(houseinfo, this_script)
-        doc.wasGeneratedBy(houseinfo, get_houses, endTime)
-        doc.wasDerivedFrom(houseinfo, resource, get_houses, get_houses, get_houses)
+        get_propetyinfo = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime, {prov.model.PROV_LABEL:'propety', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAssociatedWith(get_propetyinfo, this_script)
+        doc.usage(get_propetyinfo, resource, startTime)
+        propetyinfo = doc.entity('dat:lc546_jofranco#propetyinfo', {prov.model.PROV_LABEL:'propety Bike info', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(propetyinfo, this_script)
+        doc.wasGeneratedBy(propetyinfo, get_propetyinfo, endTime)
+        doc.wasDerivedFrom(propetyinfo, resource, get_propetyinfo, get_propetyinfo, get_propetyinfo)
         return doc
 
 
