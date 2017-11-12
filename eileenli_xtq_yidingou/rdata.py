a83386960b5077a3bb7fa87fa4e1012abf745293
Ticzu <jeremyxtq@gmail.com>
2017-11-12 14:57:29 -0500

    rdata changed
    sup brah

--- eileenli_xtq_yidingou/rdata.py
+++ eileenli_xtq_yidingou/rdata.py
@@ -8,7 +8,7 @@ import uuid
 class rdata(dml.Algorithm):
     contributor = 'eileenli_xtq_yidingou'
     reads = []
-    writes = ['eileenli_xtq_yidingou.jam', 'eileenli_xtq_yidingou.crash', 'eileenli_xtq_yidingou.MBTA', 'eileenli_xtq_yidingou.schools', 'eileenli_xtq_yidingou.hubway']
+    writes = ['eileenli_xtq_yidingou.jam', 'eileenli_xtq_yidingou.crash', 'eileenli_xtq_yidingou.schools', 'eileenli_xtq_yidingou.hubway', 'eileenli_xtq_yidingou.signals']
 
     @staticmethod
     def execute(trial = False):
@@ -20,13 +20,13 @@ class rdata(dml.Algorithm):
         repo = client.repo
         repo.authenticate('eileenli_xtq_yidingou', 'eileenli_xtq_yidingou')
 
-        url = 'http://datamechanics.io/data/eileenli_xtq_yidingou/MBTA.geojson'
+        url = 'http://datamechanics.io/data/eileenli_xtq_yidingou/Traffic_Signals.geojson'
         response = urllib.request.urlopen(url).read().decode("utf-8")
         r = json.loads(response)['features']
         s = json.dumps(r, sort_keys=True, indent=2)
-        repo.dropCollection("MBTA")
-        repo.createCollection("MBTA")
-        repo['eileenli_xtq_yidingou.MBTA'].insert_many(r)
+        repo.dropCollection("signals")
+        repo.createCollection("signals")
+        repo['eileenli_xtq_yidingou.signals'].insert_many(r)
         #repo['eileenli_xtq_yidingou.MBTA'].metadata({'complete':True})
         #print(repo['eileenli_xtq_yidingou.MBTA'].metadata())
 
@@ -107,25 +107,25 @@ class rdata(dml.Algorithm):
         
         resource_jam = doc.entity('dio:jam', {'prov:label':'Traffic Jam', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         resource_hubway = doc.entity('dio:Hubway_Stations', {'prov:label':'Hubway Stations', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
-        resource_MBTA = doc.entity('dio:MBTA', {'prov:label':'MBTA Info', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
+        resource_signals = doc.entity('dio:Traffic_Signals', {'prov:label':'Traffic Signal Info', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
         resource_crash = doc.entity('dio:crash', {'prov:label':'Crashes', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         resource_schools = doc.entity('dio:Colleges_and_Universities', {'prov:label':'Colleges_and_Universities', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'geojson'})
        
 
         get_jam = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_MBTA = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_signals = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         get_crash = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         get_hubway = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         get_schools = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
 
         doc.wasAssociatedWith(get_jam, this_script)
-        doc.wasAssociatedWith(get_MBTA, this_script)
+        doc.wasAssociatedWith(get_signals, this_script)
         doc.wasAssociatedWith(get_crash, this_script)
         doc.wasAssociatedWith(get_hubway, this_script)
         doc.wasAssociatedWith(get_schools, this_script)
 
         doc.usage(get_jam, resource_jam, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
-        doc.usage(get_MBTA, resource_MBTA, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
+        doc.usage(get_signals, resource_signals, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
         doc.usage(get_crash, resource_crash, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
         doc.usage(get_hubway, resource_hubway, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
         doc.usage(get_schools, resource_schools, startTime, None,{prov.model.PROV_TYPE:'ont:Retrieval',})
@@ -136,10 +136,10 @@ class rdata(dml.Algorithm):
         doc.wasGeneratedBy(jam, get_jam, endTime)
         doc.wasDerivedFrom(jam, resource_jam, get_jam, get_jam, get_jam)
 
-        MBTA = doc.entity('dat:eileenli_xtq_yidingou#MBTA', {prov.model.PROV_LABEL:'hubway', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(MBTA, this_script)
-        doc.wasGeneratedBy(MBTA, get_MBTA, endTime)
-        doc.wasDerivedFrom(MBTA, resource_MBTA, get_MBTA, get_MBTA, get_MBTA)
+        signals = doc.entity('dat:eileenli_xtq_yidingou#signals', {prov.model.PROV_LABEL:'signals', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(signals, this_script)
+        doc.wasGeneratedBy(signals, get_signals, endTime)
+        doc.wasDerivedFrom(signals, resource_signals, get_signals, get_signals, get_signals)
 
         crash = doc.entity('dat:eileenli_xtq_yidingou#crash', {prov.model.PROV_LABEL:'Crashes in Cambridge', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(crash, this_script)
