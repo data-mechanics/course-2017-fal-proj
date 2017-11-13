78ef2901b2f7caf2bd5862465a9d31eade40147d
FrancisZamora <franciszamor@gmail.com>
2017-11-13 18:02:26 -0500

    working on it

--- Correlation.py
+++ Correlation.py
@@ -95,34 +95,6 @@ class Correlation(dml.Algorithm):
 
     
 
-        def permute(x):
-            shuffled = [xi for xi in x]
-            shuffle(shuffled)
-            return shuffled
-
-        def avg(x): # Average
-            return sum(x)/len(x)
-
-        def stddev(x): # Standard deviation.
-            m = avg(x)
-            return sqrt(sum([(xi-m)**2 for xi in x])/len(x))
-
-        def cov(x, y): # Covariance.
-            return sum([(xi-avg(x))*(yi-avg(y)) for (xi,yi) in zip(x,y)])/len(x)
-
-        def corr(x, y): # Correlation coefficient.
-            if stddev(x)*stddev(y) != 0:
-                return cov(x, y)/(stddev(x)*stddev(y))
-
-        def p(x, y):
-            c0 = corr(x, y)
-            corrs = []
-            for k in range(0, 2000):
-                y_permuted = permute(y)
-                corrs.append(corr(x, y_permuted))
-            return len([c for c in corrs if abs(c) > c0])/len(corrs)
-
-        print(p(x1,x2))
 
 
 
