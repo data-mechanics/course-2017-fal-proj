db45fea633e403b6e98a44a370cd3b6acf67d155
FrancisZamora <franciszamor@gmail.com>
2017-11-12 22:48:23 -0500

    provenance

--- Correlation.py
+++ Correlation.py
@@ -9,13 +9,15 @@ import geojson
 #from tqdm import tqdm
 import pdb
 import scipy.stats
+from random import shuffle
+from math import sqrt
 
-class correlation(dml.Algorithm):
+class Correlation(dml.Algorithm):
     contributor = 'francisz_jrashaan'
     
-    reads = ['francisz_jrashaan.crime']
+    reads = ['francisz_jrashaan.neighborhoodScores']
     
-    writes = ['francisz_jrashaan.crimeData']
+    writes = ['francisz_jrashaan.correlationScore']
     
     
     @staticmethod
@@ -24,8 +26,11 @@ class correlation(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('francisz_jrashaan','francisz_jrashaan')
+        repo.dropCollection("correlationScore")
+        repo.createCollection("correlationScore")
+        scores =  repo.francisz_jrashaan.neighborhoodScores.find()
 
-        scores = [('North End', [0, 3, 236, 240]), ('Bay Village', [0, 0, 24, 42]), ('East Boston', [0, 19, 222, 3544]), ('Leather District', [8, 8, 34, 43]), ('Allston', [0, 1, 1888, 1994]), ('Hyde Park', [0, 0, 569, 1163]), ('Roslindale', [0, 0, 450, 608]), ('Charlestown', [0, 7, 189, 455]), ('Back Bay', [4, 17, 432, 817]), ('South End', [0, 0, 116, 150]), ('Downtown', [4, 33, 160, 420]), ('Dorchester', [0, 7, 1382, 3710]), ('South Boston Waterfront', [15, 7, 102, 222]), ('West Roxbury', [0, 0, 559, 708]), ('Longwood Medical Area', [0, 11, 136, 154]), ('Mission Hill', [0, 11, 135, 161]), ('Roxbury', [0, 7, 315, 525]), ('Beacon Hill', [1, 16, 149, 391]), ('Mattapan', [0, 0, 348, 627]), ('Harbor Islands', [0, 0, 0, 155]), ('Brighton', [0, 0, 983, 1466]), ('South Boston', [0, 1, 410, 1061]), ('West End', [0, 5, 387, 549]), ('Fenway', [4, 21, 893, 1034]), ('Chinatown', [11, 21, 74, 112]), ('Jamaica Plain', [0, 0, 356, 919])]
+        #scores = [('North End', [0, 3, 236, 240]), ('Bay Village', [0, 0, 24, 42]), ('East Boston', [0, 19, 222, 3544]), ('Leather District', [8, 8, 34, 43]), ('Allston', [0, 1, 1888, 1994]), ('Hyde Park', [0, 0, 569, 1163]), ('Roslindale', [0, 0, 450, 608]), ('Charlestown', [0, 7, 189, 455]), ('Back Bay', [4, 17, 432, 817]), ('South End', [0, 0, 116, 150]), ('Downtown', [4, 33, 160, 420]), ('Dorchester', [0, 7, 1382, 3710]), ('South Boston Waterfront', [15, 7, 102, 222]), ('West Roxbury', [0, 0, 559, 708]), ('Longwood Medical Area', [0, 11, 136, 154]), ('Mission Hill', [0, 11, 135, 161]), ('Roxbury', [0, 7, 315, 525]), ('Beacon Hill', [1, 16, 149, 391]), ('Mattapan', [0, 0, 348, 627]), ('Harbor Islands', [0, 0, 0, 155]), ('Brighton', [0, 0, 983, 1466]), ('South Boston', [0, 1, 410, 1061]), ('West End', [0, 5, 387, 549]), ('Fenway', [4, 21, 893, 1034]), ('Chinatown', [11, 21, 74, 112]), ('Jamaica Plain', [0, 0, 356, 919])]
         relationdata = []
         Correlations = []
 
@@ -36,7 +41,42 @@ class correlation(dml.Algorithm):
 
         x = [xi for (xi, yi) in relationdata]
         y = [yi for (xi, yi) in relationdata]
-        print(scipy.stats.pearsonr(x, y))
+
+    
+
+    def permute(x):
+        shuffled = [xi for xi in x]
+        shuffle(shuffled)
+        return shuffled
+
+    def avg(x): # Average
+        return sum(x)/len(x)
+
+    def stddev(x): # Standard deviation.
+        m = avg(x)
+        return sqrt(sum([(xi-m)**2 for xi in x])/len(x))
+
+    def cov(x, y): # Covariance.
+        return sum([(xi-avg(x))*(yi-avg(y)) for (xi,yi) in zip(x,y)])/len(x)
+
+    def corr(x, y): # Correlation coefficient.
+        if stddev(x)*stddev(y) != 0:
+            return cov(x, y)/(stddev(x)*stddev(y))
+
+    def p(x, y):
+        c0 = corr(x, y)
+        corrs = []
+        for k in range(0, 2000):
+            y_permuted = permute(y)
+            corrs.append(corr(x, y_permuted))
+        return len([c for c in corrs if abs(c) > c0])/len(corrs)
+    score = []
+    score.append(p(x,y))
+    print(score[0])
+    repo['francisz_jrashaan.correlationScore'].insert_many(score)
+    repo['francisz_jrashaan.correlationScore'].metadata({'complete':True})
+
+
 
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
@@ -54,61 +94,24 @@ class correlation(dml.Algorithm):
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
-        doc.add_namespace('cam','https://data.cambridgema.gov/api/views/')
+        doc.add_namespace('bdp', 'http://bostonopendata-boston.opendata.arcgis.com/')
         
         
-        this_script = doc.agent('alg:francisz_jrashaan#fzjr_retrievalalgorithm', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource_chargeStations = doc.entity('bdp:12cb3883-56f5-47de-afa5-3b1cf61b257b', {'prov:label':'chargeStations', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        resource_Hubways = doc.entity('bdp:c2fcc1e3-c38f-44ad-a0cf-e5ea2a6585b5', {'prov:label':'Hubways', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        resource_bikeNetwork = doc.entity('cam:srp4-fhjz/rows.json', {'prov:label':'bikeNetworks', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        resource_capopulation = doc.entity('cam:r4pm-qqje/rows', {'prov:label':'Cambridge Population Density', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        resource_openspace = doc.entity('bdp:769c0a21-9e35-48de-a7b0-2b7dfdefd35e', {'prov:label':'Openspace', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        this_script = doc.agent('alg:francisz_jrashaan#Correlation', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource_neighborhoodscores = doc.entity('dat:francisz_jrashaan#NeighborhoodScores', {'prov:label':'Neighborhood Scores', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'BSON'})
         
-        get_chargeStations = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_Hubways = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_bikeNetworks = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_capopulation = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        get_openspace = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(get_chargeStations, this_script)
-        doc.wasAssociatedWith(get_Hubways, this_script)
-        doc.wasAssociatedWith(get_bikeNetworks, this_script)
-        doc.wasAssociatedWith(get_capopulation, this_script)
-        doc.wasAssociatedWith(get_openspace, this_script)
-        doc.usage(get_chargeStations, resource_chargeStations, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval'})
-        doc.usage(get_Hubways, resource_Hubways, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval'})
-        doc.usage(get_bikeNetworks, resource_bikeNetwork, startTime, None,
-                            {prov.model.PROV_TYPE:'ont:Retrieval'})
-        doc.usage(get_capopulation, resource_capopulation, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval'})
-        doc.usage(get_openspace, resource_openspace, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval'})
-        chargeStations = doc.entity('dat:francisz_jrashaan#chargeStations', {prov.model.PROV_LABEL:'chargeStations', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(chargeStations, this_script)
-        doc.wasGeneratedBy(crime, get_chargeStations, endTime)
-        doc.wasDerivedFrom(crime, resource_chargeStations, get_chargeStations, get_chargeStations, get_chargeStations)
+        compute_correlation = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        
+
+        doc.wasAssociatedWith(compute_correlation, this_script)
+       
+        doc.usage(compute_correlation, resource_neighborhood, startTime, None, {prov.model.PROV_TYPE:'ont:Used for Computation'})
+     
                   
-        Hubways = doc.entity('dat:francisz_jrashaan#Hubways', {prov.model.PROV_LABEL:'Hubways', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(Hubways, this_script)
-        doc.wasGeneratedBy(Hubways, get_streetlights, endTime)
-        doc.wasDerivedFrom(Hubways, resource_Hubways, get_Hubways, get_Hubways, get_Hubways)
-                  
-        bikeNetwork = doc.entity('dat:francisz_jrashaan#bikeNetwork', {prov.model.PROV_LABEL:'bikeNetwork', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(bikeNetwork, this_script)
-        doc.wasGeneratedBy(bikeNetwork, get_bikeNetworks, endTime)
-        doc.wasDerivedFrom(bikeNetwork, resource_bikeNetwork, get_bikeNetworks, get_bikeNetworks, get_bikeNetworks)
-                  
-        capopulation = doc.entity('dat:francisz_jrashaan#capopulation', {prov.model.PROV_LABEL:'capopulation', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(capopulation, this_script)
-        doc.wasGeneratedBy(capopulation, get_capopulation, endTime)
-        doc.wasDerivedFrom(capopulation, resource_capopulation, get_capopulation, get_capopulation, get_capopulation)
-                  
-        openspace = doc.entity('dat:francisz_jrashaan#openspace', {prov.model.PROV_LABEL:'openspace', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(openspace, this_script)
-        doc.wasGeneratedBy(openspace, get_openspace, endTime)
-        doc.wasDerivedFrom(openspace, resource_openspace, get_openspace, get_openspace, get_openspace)
+        correlationscore = doc.entity('dat:francisz_jrashaan#correlationScore', {prov.model.PROV_LABEL:'Correlation Score', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(correlationScore, this_script)
+        doc.wasGeneratedBy(correlationScore, compute_correlation, endTime)
+        doc.wasDerivedFrom(correlationScore, resource_neighborhood, compute_correlation, compute_correlation, compute_correlation)
                   
         repo.logout()
                   
