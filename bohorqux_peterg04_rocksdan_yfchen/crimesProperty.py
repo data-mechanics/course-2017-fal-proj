74fd70cc6da4e247e6c24426b932b236162ba658
Xavier Bohorquez <bohorqux@bu.edu>
2017-11-13 20:56:16 -0500

    Updated crimesProperty file, must fix runtime issue

--- bohorqux_peterg04_rocksdan_yfchen/crimesProperty.py
+++ bohorqux_peterg04_rocksdan_yfchen/crimesProperty.py
@@ -6,9 +6,9 @@ import datetime
 import uuid
 
 class crimesProperty(dml.Algorithm):
-    contributor = 'bohorqux_rocksdan'
-    reads = ['bohorqux_rocksdan.properties', 'bohorqux_rocksdan.crimes']
-    writes = ['bohorqux_rocksdan.property_crimes']
+    contributor = 'bohorqux_peterg04_rocksdan_yfchen'
+    reads = ['bohorqux_peterg04_rocksdan_yfchen.properties', 'bohorqux_peterg04_rocksdan_yfchen.crimes']
+    writes = ['bohorqux_peterg04_rocksdan_yfchen.property_crimes']
 
     @staticmethod
     def execute(trial = False):
@@ -18,21 +18,24 @@ class crimesProperty(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('bohorqux_rocksdan', 'bohorqux_rocksdan')
+        repo.authenticate('bohorqux_peterg04_rocksdan_yfchen', 'bohorqux_peterg04_rocksdan_yfchen')
 
         # 244 Streets in common. p has less streets than c
-        properties = repo['bohorqux_rocksdan.properties']
-        crimes = repo['bohorqux_rocksdan.crimes']
+        properties = repo['bohorqux_peterg04_rocksdan_yfchen.properties']
+        crimes = repo['bohorqux_peterg04_rocksdan_yfchen.crimes']
 
         property_reports = []
         crime_reports = []
         intersect = []
 
         #Create distinct list of streets corresponding to each database
+        print("Parsing houses...")
         for houses in properties.find():
-            if houses["ST_NAME"] not in property_reports:
-                property_reports.append(houses["ST_NAME"])
+            street = houses["ST_NAME"] + " " + houses["ST_NAME_SUF"]
+            if street not in property_reports:
+                property_reports.append(street)
 
+        print("Parsing Crimes...")
         for reports in crimes.find():
             if reports["STREET"] not in crime_reports:
                 crime_reports.append(reports["STREET"])
@@ -42,6 +45,8 @@ class crimesProperty(dml.Algorithm):
             if street in crime_reports:
                 intersect.append(street)
 
+        print("Shared Data:", str(len(intersect)))
+        
         #Create dictionary of streets, setting None as initial values
         properties_crimes = dict().fromkeys(intersect)
 
@@ -57,28 +62,34 @@ class crimesProperty(dml.Algorithm):
 
         indice = 0
         for key in properties_crimes: #iterating through all street names
+            
             #Insert Crimes data related to this street in this key
             print(key)
+            
             properties_crimes[key] = {"Crimes": list(), "Properties": list()}
             for reports in crimes.find():
-                if reports["STREET"] == key:
+                if reports["STREET"] == key or reports["STREET"] in key:
                     properties_crimes[key]["Crimes"].append({"Offense Code": reports["OFFENSE_CODE_GROUP"], "Description": reports["OFFENSE_DESCRIPTION"], "Shooting": reports["SHOOTING"],                     "DateTime": reports["OCCURRED_ON_DATE"], "Location": reports["Location"]})
             print('*****' + key + " CRIMES COMPLETE")
+            
             #Insert Properties data related to this street in this key
             for reports in properties.find():
-                if reports["ST_NAME"] == key:
+                if reports["ST_NAME"] == key or reports["ST_NAME"] in key:
                     properties_crimes[key]["Properties"].append({"ZIPCODE": reports["ZIPCODE"], "AVG_LAND": reports["AV_LAND"], "AVG_BLDG": reports["AV_BLDG"],
                                                                  "AVG_TOTAL": reports["AV_TOTAL"], "GROSS_TAX": reports["GROSS_TAX"], "Floors": reports["NUM_FLOORS"],
                                                                  "Year Built": reports["YR_BUILT"]})
+                    
             indice += 1
             print('-----' + key + " PROPERTIES COMPLETE" + "\t" + str(indice))
+            if indice == 10:
+                break
         
         p_c = list()
         p_c.append(properties_crimes)
         
         repo.dropCollection("property_crimes")
         repo.createCollection("property_crimes")
-        repo['bohorqux_rocksdan.property_crimes'].insert_many(p_c)
+        repo['bohorqux_peterg04_rocksdan_yfchen.property_crimes'].insert_many(p_c)
         
         repo.logout()
 
@@ -97,14 +108,14 @@ class crimesProperty(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('bohorqux_rocksdan', 'bohorqux_rocksdan')
+        repo.authenticate('bohorqux_peterg04_rocksdan_yfchen', 'bohorqux_peterg04_rocksdan_yfchen')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-        this_script = doc.agent('alg:bohorqux_rocksdan#crimesProperty', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        this_script = doc.agent('alg:bohorqux_peterg04_rocksdan_yfchen#crimesProperty', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         get_properties = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         doc.wasAssociatedWith(get_properties, this_script)
@@ -113,7 +124,7 @@ class crimesProperty(dml.Algorithm):
                    'ont:Query':'?Street=TREMONT ST&$select=Street'
                   }
                   )
-        properties = doc.entity('dat:bohorqux_rocksdan#property_crimes', {prov.model.PROV_LABEL:'Property Values', prov.model.PROV_TYPE:'ont:DataSet'})
+        properties = doc.entity('dat:bohorqux_peterg04_rocksdan_yfchen#property_crimes', {prov.model.PROV_LABEL:'Property Values', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(properties, this_script)
         doc.wasGeneratedBy(properties, get_properties, endTime)
         doc.wasDerivedFrom(properties, resource, get_properties, get_properties, get_properties)
@@ -122,7 +133,7 @@ class crimesProperty(dml.Algorithm):
                   
         return doc
 
-#crimesProperty.execute()
+crimesProperty.execute()
 #doc = crimesProperty.provenance()
 #print(doc.get_provn())
 #print(json.dumps(json.loads(doc.serialize()), indent=4))
