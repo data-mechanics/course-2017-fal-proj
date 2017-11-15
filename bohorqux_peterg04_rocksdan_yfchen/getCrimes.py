d92728393b39f74be8f575589b645dead4202b2b
Xavier Bohorquez <bohorqux@bu.edu>
2017-11-14 23:33:11 -0500

    fixed indent error in getCrimes

--- bohorqux_peterg04_rocksdan_yfchen/getCrimes.py
+++ bohorqux_peterg04_rocksdan_yfchen/getCrimes.py
@@ -54,11 +54,9 @@ class getCrimes(dml.Algorithm):
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-	doc.add_namespace('cd', 'https://data.boston.gov/export/12c/b38/12cb3883-56f5-47de-afa5-3b1cf61b257b.json')
-
 		
         this_script = doc.agent('alg:bohorqux_peterg04_rocksdan_yfchen#getCrimes', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        resource = doc.entity('cd:crimes, {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        resource = doc.entity('cd:crimes', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         get_crimes = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         doc.wasAssociatedWith(get_crimes, this_script)
         doc.usage(get_crimes, resource, startTime, None,
