69ef92826a9740497f7f0a4a3ebea22f9e5a04d7
Daniel Qiu <rocksdan@bu.edu>
2017-11-14 22:53:07 -0500

    crimes pov

--- bohorqux_peterg04_rocksdan_yfchen/getCrimes.py
+++ bohorqux_peterg04_rocksdan_yfchen/getCrimes.py
@@ -54,7 +54,6 @@ class getCrimes(dml.Algorithm):
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-
 		doc.add_namespace('cd', 'https://data.boston.gov/export/12c/b38/12cb3883-56f5-47de-afa5-3b1cf61b257b.json')
 
 		
