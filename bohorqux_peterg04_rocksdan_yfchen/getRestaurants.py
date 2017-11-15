760ef564d8c7666c8228bd17c8a0f5b9ed0df02c
BenChenBU <yfchen@bu.edu>
2017-11-15 00:21:31 -0500

    random indents and github reverts...

--- bohorqux_peterg04_rocksdan_yfchen/getRestaurants.py
+++ bohorqux_peterg04_rocksdan_yfchen/getRestaurants.py
@@ -52,26 +52,26 @@ class getRestaurants(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('bohorqux_peterg04_rocksdan_yfchen', 'bohorqux_peterg04_rocksdan_yfchen')
-#         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
-#         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
-#         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
-#         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-#         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
-# 
-#         this_script = doc.agent('alg:bohorqux_peterg04_rocksdan_yfchen#getRestaurants', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-#         resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-#         get_Restaurants = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-#         doc.wasAssociatedWith(get_Restaurants, this_script)
-#         doc.usage(get_Restaurants, resource, startTime, None,
-#                   {prov.model.PROV_TYPE:'ont:Retrieval',
-#                   'ont:Query':'?type=BostonLife+Restaurants&$select=type,latitude,longitude,OPEN_DT'
-#                   }
-#                   )
-# 
-#         Restaurants = doc.entity('dat:bohorqux_peterg04_rocksdan_yfchen#Restaurants', {prov.model.PROV_LABEL:'BostonLife Restaurants', prov.model.PROV_TYPE:'ont:DataSet'})
-#         doc.wasAttributedTo(Restaurants, this_script)
-#         doc.wasGeneratedBy(Restaurants, getRestaurants, endTime)
-#         doc.wasDerivedFrom(Restaurants, resource, getRestaurants, getRestaurants, getRestaurants)
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+ 
+        this_script = doc.agent('alg:bohorqux_peterg04_rocksdan_yfchen#getRestaurants', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        get_Restaurants = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_Restaurants, this_script)
+        doc.usage(get_Restaurants, resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval',
+                  'ont:Query':'?type=BostonLife+Restaurants&$select=type,latitude,longitude,OPEN_DT'
+                  }
+                  )
+ 
+        Restaurants = doc.entity('dat:bohorqux_peterg04_rocksdan_yfchen#Restaurants', {prov.model.PROV_LABEL:'BostonLife Restaurants', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(Restaurants, this_script)
+        doc.wasGeneratedBy(Restaurants, getRestaurants, endTime)
+        doc.wasDerivedFrom(Restaurants, resource, getRestaurants, getRestaurants, getRestaurants)
 
         repo.logout()
                   
