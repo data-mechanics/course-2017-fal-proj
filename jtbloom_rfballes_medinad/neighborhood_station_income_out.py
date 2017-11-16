5036058e14885be1782a135764f4636bfbf11356
Jake Bloomfeld <jbloomfeld@gmail.com>
2017-11-15 19:04:35 -0500

    Update neighborhood_station_income_out.py

--- jtbloom_rfballes_medinad/neighborhood_station_income_out.py
+++ jtbloom_rfballes_medinad/neighborhood_station_income_out.py
@@ -61,42 +61,43 @@ class neighborhood_station_income_out(dml.Algorithm):
 	@staticmethod
 	def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
 		pass
-		pass
-
-        # Set up the database connection.
-        client = dml.pymongo.MongoClient()
-        repo = client.repo
-        repo.authenticate('jtbloom_rfballes_medinad', 'jtbloom_rfballes_medinad')
-        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
-        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
-        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
-        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
-        doc.add_namespace('dm', 'http://datamechanics.io/data/jb_rfb_dm_proj2data/')
-        
-        this_script = doc.agent('dm:jtbloom_rfballes_medinad#neighborhood_station_income_out', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
-        station_resource = doc.entity('dm:stationsByneighborhood', {'prov:label':'Hubway Stations by Neighborhood', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'DataSet'})
-        income_resource = doc.entity('dm:neighborhood_income_final', {'prov:label':'Income by Neighborhood', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'DataSet'})
 
 
-        this_run = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        doc.wasAssociatedWith(this_run, this_script)
+      		# Set up the database connection.
+       		client = dml.pymongo.MongoClient()
+		repo = client.repo
+       		repo.authenticate('jtbloom_rfballes_medinad', 'jtbloom_rfballes_medinad')
+        	doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+		doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+		doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+		doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+		doc.add_namespace('dm', 'http://datamechanics.io/data/jb_rfb_dm_proj2data/')
 
-        doc.usage(this_run, station_resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval'})
+		this_script = doc.agent('dm:jtbloom_rfballes_medinad#neighborhood_station_income_out', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+		station_resource = doc.entity('dm:stationsByneighborhood', {'prov:label':'Hubway Stations by Neighborhood', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'DataSet'})
+		income_resource = doc.entity('dm:neighborhood_income_final', {'prov:label':'Income by Neighborhood', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'DataSet'})
 
 
-        doc.usage(this_run, income_resource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval'})
+		this_run = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+		doc.wasAssociatedWith(this_run, this_script)
 
-      
-        doc.wasAttributedTo(neighborhood_station_income_out, this_script)
-        doc.wasGeneratedBy(neighborhood_station_income_out, this_run, endTime)
-       
-        doc.wasDerivedFrom(neighborhood_station_income_out, station_resource, this_run, this_run, this_run)
-        doc.wasDerivedFrom(neighborhood_station_income_out, income_resource, this_run, this_run, this_run)
+		doc.usage(this_run, station_resource, startTime, None,
+			  {prov.model.PROV_TYPE:'ont:Retrieval'})
 
-        repo.logout()
 
-        return doc
+		doc.usage(this_run, income_resource, startTime, None,
+			  {prov.model.PROV_TYPE:'ont:Retrieval'})
+
+
+		doc.wasAttributedTo(neighborhood_station_income_out, this_script)
+		doc.wasGeneratedBy(neighborhood_station_income_out, this_run, endTime)
+
+		doc.wasDerivedFrom(neighborhood_station_income_out, station_resource, this_run, this_run, this_run)
+		doc.wasDerivedFrom(neighborhood_station_income_out, income_resource, this_run, this_run, this_run)
+
+		repo.logout()
+
+		return doc
+
 
 #neighborhood_station_income_out.execute()
