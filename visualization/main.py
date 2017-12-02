16bb255ab4720727828f7ef46599b3247a3f2f7c
Rayliu927 <mac@dhcp-wifi-8021x-155-41-114-11.bu.edu>
2017-12-02 15:09:10 -0500

    update drop down

--- visualization/main.py
+++ visualization/main.py
@@ -20,9 +20,43 @@ def register_user():
 	except:
 		print("couldn't find all tokens")
 		return render_template('index.html')
-		
+
 	if drop == "Original":
-		
+		pLocation = repo['cyyan_liuzirui_yjunchoi_yzhang71.pollingLocation'].find()
+		Loc = []
+		for o in pLocation:
+		    for i in range(0,len(o['coordinates'])):
+		        Loc.append(o['coordinates'][i])
+	elif drop == "Transit":
+		public = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByPublicT'].find()
+		publicLoc = {}
+		Loc = []
+		publicLoc[0] = public[0]
+		for i in range(1, len(public[0])):
+			for o in publicLoc[0][str(i)]:
+				Loc.append(o)
+	elif drop == "MBTA":
+		optByMBTA = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByMBTA'].find()
+		MBTALoc = {}
+		Loc = []
+		MBTALoc[0] = optByMBTA[0]
+		for i in range(1, len(optByMBTA[0])):
+			for o in MBTALoc[0][str(i)]:
+				Loc.append(o)
+	elif drop == "BUS":
+		optByBusstop = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByBusstop'].find()
+		BUSLoc = {}
+		Loc = []
+		BUSLoc[0] = optByBusstop[0]
+		for i in range(1, len(optByBusstop[0])):
+			for o in BUSLoc[0][str(i)]:
+				Loc.append(o)
+	else:
+		print("Error")
+		return render_template('index.html')
+	
+
+
 
 
 
@@ -32,3 +66,18 @@ def score():
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
