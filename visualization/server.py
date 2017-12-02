ba6c6d1f7b2a42b31ce8bd3d2570cdc4bf475491
Steven Brzozowski <sbrz@bu.edu>
2017-12-02 18:44:04 -0500

    Added support for submitted desired number of means/routes

--- visualization/server.py
+++ visualization/server.py
@@ -9,13 +9,23 @@ def index():
 
 
 @app.route('/means')
-def data():
+def means():
     # here we want to get the value of user (i.e. ?means=some-value)
     means = int(request.args.get('means'))
     if means > 0:
         return "You selected {} means".format(means)
     else:
         return "Please enter a valid number of means"
+
+
+@app.route('/emergency_routes')
+def routes():
+    # here we want to get the value of user (i.e. ?means=some-value)
+    routes = int(request.args.get('routes'))
+    if routes > 0:
+        return "You selected {} routes".format(routes)
+    else:
+        return "Please enter a valid number of routes"
 
 
 if __name__ == "__main__":
