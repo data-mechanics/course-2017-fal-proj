84d7b764767fff0d163bdce240447722c1a23d54
Rayliu927 <mac@dhcp-wifi-8021x-155-41-114-11.bu.edu>
2017-12-02 14:19:11 -0500

    uploading html

--- visualization/main.py
+++ visualization/main.py
@@ -8,10 +8,24 @@ client = pymongo.MongoClient()
 repo = client.repo
 repo.authenticate('cyyan_liuzirui_yjunchoi_yzhang71', 'cyyan_liuzirui_yjunchoi_yzhang71')
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def hello():
-    return render_template('hello.html')
+    return render_template('index.html')
+
+@app.route('/',methods=['POST'])
+def register_user():
+	try:
+		wardname = request.form.get('ward')
+		drop =request.form.get('dropdown')
+		print("ok")
+		return render_template('index.html')
+	except:
+		print("couldn't find all tokens")
+		return render_template('index.html')
 
 @app.route('/score')
 def score():
     return render_template('score.html')
+
+if __name__ == '__main__':
+    app.run()
