53dc2d45271ee312f7180b1048a4d0bc4d7dabe3
Rayliu927 <mac@dhcp-wifi-8021x-155-41-114-11.bu.edu>
2017-12-03 13:58:46 -0500

    update scoring board function

--- visualization/main.py
+++ visualization/main.py
@@ -2,6 +2,8 @@ import flask
 from flask import Flask, render_template, jsonify, request
 import pymongo
 
+# To run the program: FLASK_APP=main.py flask run
+
 app = Flask(__name__)
 client = pymongo.MongoClient()
 repo = client.repo
@@ -45,6 +47,47 @@ def map():
         return jsonify(loc)
     else:
         return render_template('index.html')
+
+@app.route('/score', methods = ['GET', 'POST'])
+def score_board():
+    if request.method == 'POST':
+        drop1 = request.form.get('dropdown1')
+        drop2 = request.form.get('dropdown2')
+        print("this is drop1", drop1)
+        print("this is drop2", drop2)
+
+        NewDrop1 = score_drop(drop1)
+        NewDrop2 = score_drop(drop2)
+
+        if NewDrop1 == "error" or NewDrop2 == "error":
+            print('Something wrong with dropdown')
+        else:
+            drop1 = NewDrop1
+            drop2 = NewDrop2
+
+        scoring = repo['cyyan_liuzirui_yjunchoi_yzhang71.scoringLocation'].find()
+        s = {}
+        s[drop1] = scoring[0][drop1]
+        s[drop2] = scoring[0][drop2]
+        print(s)
+        return render_template('score.html') 
+
+    else:
+        return render_template('score.html')
+
+def score_drop(drop):
+    if drop == "Original":
+        return "pollingLocation"
+    elif drop == "Transit":
+        return "optByPublicT"
+    elif drop == "MBTA":
+        return "optByMBTA"
+    elif drop == "BUS":
+        return "optByBusstop"
+    else:
+        return "error"
+
+
 
 if __name__ == '__main__':
     app.run()
