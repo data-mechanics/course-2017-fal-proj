61a7929d63a1a5869ee0972d735477fe83616f24
Young Jun Choi <yjunchoi93@gmail.com>
2017-12-03 21:16:12 -0500

    Fix Flask

--- visualization/main.py
+++ visualization/main.py
@@ -1,6 +1,8 @@
 import flask
+import json
 from flask import Flask, render_template, jsonify, request
 import pymongo
+
 
 # To run the program: FLASK_APP=main.py flask run
 
@@ -9,44 +11,65 @@ client = pymongo.MongoClient()
 repo = client.repo
 repo.authenticate('cyyan_liuzirui_yjunchoi_yzhang71', 'cyyan_liuzirui_yjunchoi_yzhang71')
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/')
+def hello():
+    return render_template('home.html')
+
+@app.route('/marker')
+def getMarker():
+    marker = json.load(open('latlng.json', 'r'))
+    return jsonify(marker)
+
+@app.route('/map', methods = ['POST'])
 def map():
-    if request.method == 'POST':
-        wardname = request.form.get('ward')
-        drop = request.form.get('dropdown')
+    wardname = request.form.get('ward')
+    drop = request.form.get('dropdown')
 
-        loc = []
-        dLoc = {}
+    l = []
+    loc = []
+    dLoc = {}
 
-        if drop == 'Original':
-            pLocation = repo['cyyan_liuzirui_yjunchoi_yzhang71.pollingLocation'].find()
-            for p in pLocation:
-                for i in range(len(p['coordinates'])):
-                    loc.append(p['coordinates'][i])
-        elif drop == 'Transit':
-            public = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByPublicT'].find()
-            dLoc[0] = public[0]
-            for i in range(1, len(public[0])):
-                for l in dLoc[0][str(i)]:
-                    loc.append(l)
-        elif drop == "MBTA":
-            public = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByMBTA'].find()
-            dLoc[0] = public[0]
-            for i in range(1, len(public[0])):
-                for l in dLoc[0][str(i)]:
-                    loc.append(l)
-        elif drop == "BUS":
-            public = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByBusstop'].find()
-            dLoc[0] = public[0]
-            for i in range(1, len(public[0])):
-                for l in dLoc[0][str(i)]:
-                    loc.append(l)
-        else:
-            print('Something wrong with dropdown')
-
-        return jsonify(loc)
+    if drop == 'Original':
+        pLocation = repo['cyyan_liuzirui_yjunchoi_yzhang71.pollingLocation'].find()
+        for p in pLocation:
+            for i in range(len(p['coordinates'])):
+                l = [p['coordinates'][i][1],  p['coordinates'][i][0]]
+                loc.append(l)
+    elif drop == 'Transit':
+        public = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByPublicT'].find()
+        dLoc[0] = public[0]
+        for i in range(1, len(public[0])):
+            for o in dLoc[0][str(i)]:
+                l = [o[1], o[0]]
+                loc.append(l)
+    elif drop == "MBTA":
+        public = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByMBTA'].find()
+        dLoc[0] = public[0]
+        for i in range(1, len(public[0])):
+            for o in dLoc[0][str(i)]:
+                l = [o[1], o[0]]
+                loc.append(l)
+    elif drop == "BUS":
+        public = repo['cyyan_liuzirui_yjunchoi_yzhang71.optByBusstop'].find()
+        dLoc[0] = public[0]
+        for i in range(1, len(public[0])):
+            for o in dLoc[0][str(i)]:
+                l = [o[1], o[0]]
+                loc.append(l)
     else:
-        return render_template('index.html')
+        print('Something wrong with dropdown')
+
+    result = {}
+
+    for i in range(255):
+        result[str(i)] = loc[i]
+
+    with open('latlng.json', 'w') as makeFile:
+        json.dump(result, makeFile)
+
+    marker = getMarker()
+    return render_template('map.html')
+
 
 @app.route('/score', methods = ['GET', 'POST'])
 def score_board():
@@ -71,7 +94,7 @@ def score_board():
         s[1] = dict_to_list(scoring[0][drop2], drop2)
         # print(type(scoring[0][drop1]))
         print(s[0])
-        return render_template('score.html', message = s) 
+        return render_template('score.html', message = s)
 
     else:
         return render_template('score.html')
