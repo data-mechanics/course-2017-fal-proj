5f7459108f8fc9466bb045daa93ecc0b16ba487a
Ben Gaudiosi <bgaudiosi531@gmail.com>
2017-12-05 18:35:24 -0500

    added zipcode query

--- raykatz_nedg_gaudiosi/app/app.py
+++ raykatz_nedg_gaudiosi/app/app.py
@@ -4,14 +4,21 @@ from flask import Flask, Response, request, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import flask.ext.login as flask_login
 
+import json
+
 app = Flask(__name__)
 mongo = PyMongo(app)
 app.secret_key = 'isok'
+
+def get_zipinfo(zipcode):
+    info = mongo.repo.raykatz_nedg_gaudiosi.zipcode_info.find({'zipcode':zipcode})
+    return info
 
 # index page
 @app.route("/", methods=['GET'])
 def welcome():
     return render_template('index.html')
+
 @app.route('/corrfinder', methods=['GET'])
 def corrfinder():
     return render_template('corrfinder.html')    
