c7301d978c7a9c82395e742be8bbf9d7e8d521ec
FrancisZamora <franciszamor@gmail.com>
2017-12-11 00:53:18 -0500

    templates

--- francisz_jrashaan/app.py
+++ francisz_jrashaan/app.py
@@ -1,11 +1,11 @@
 import jsonschema
 from flask import Flask, jsonify, abort, make_response, request, render_template
-from flask.ext.httpauth import HTTPBasicAuth
-from flask.ext.pymongo import PyMongo
+from flask_httpauth import HTTPBasicAuth
+from flask_pymongo import PyMongo
 import dml
 
 app = Flask(__name__)
-
+'''
 app.config['MONGO_DBNAME'] = 'repo'
 app.config['MONGO_USERNAME'] = 'francisz_jrashaan'
 app.config['MONGO_PASSWORD'] = 'francisz_jrashaan'
@@ -13,22 +13,25 @@ app.config['MONGO_PASSWORD'] = 'francisz_jrashaan'
 mongo = PyMongo(app, config_prefix='MONGO')
 
 auth = HTTPBasicAuth()
-
+'''
 @app.route('/')
 def index():
     return render_template('index.html')
-
+'''
 @app.route('/score', methods=["GET"])
 def get_big_belly():
     client = dml.pymongo.MongoClient()
     repo = client.repo
-    repo.authenticate('andradej_chojoe', 'andradej_chojoe')
+    repo.authenticate('francisz_jrashaan', 'francisz_jrashaan')
     
-    bigbellyinfo = repo['andradej_chojoe.bigbelly_transf'].find()
+    scores = repo['francisz_jrashaan.scores'].find()
     
-    bigbelly_entries = []
-    for data in bigbellyinfo:
-        bigbelly_entries.append({ 'location': data['location'], 'count': data['count'], 'percentage': data['percentage']})
+    scoreArray = []
+    for score in scores:
+        scoreArray.append({ 'location': data['location'], 'count': data['count'], 'percentage': data['percentage']})
     
     return jsonify({'results': bigbelly_entries})
+'''
+if __name__ == '__main__':
+    app.run(port=3000)
 
