feceae484f40cceb120dc7019c362c651945299b
joseph-caluag <jcaluag@bu.edu>
2017-12-12 20:38:53 -0500

    invalid input exception

--- alanbur_aquan_erj826_jcaluag/app.py
+++ alanbur_aquan_erj826_jcaluag/app.py
@@ -13,13 +13,18 @@ def main():
 @app.route('/kmeans', methods=["GET","POST"])
 def run_kmeans():
     if request.method == "POST":
-        distance=float(request.form['distance'])
-        kmeanFinder=find_kmeans.FindKMeans(distance)
-        toggle = request.form['option']
-        if toggle == 'average':
-            results=kmeanFinder.execute(toggle = True)
-        else:
-            results = kmeanFinder.execute(toggle=False)
+        try:
+            distance=float(request.form['distance'])
+            toggle = request.form['option']
+            kmeanFinder=find_kmeans.FindKMeans(distance)
+            if toggle == 'average':
+                results=kmeanFinder.execute(toggle = True)
+            else:
+                results = kmeanFinder.execute(toggle=False)
+        except:
+            return render_template('index.html',msg = 'Invalid Input')
+
+
     else:
         return render_template('index.html')
 
